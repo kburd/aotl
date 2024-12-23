530ee48ddd842ed69f7f9a3b0fd08e0900f7103a
kburd <kalebmburd@gmail.com>
2024-12-23 17:20:58 -0500

    adding gpio and signal processing logic

--- test.py
+++ test.py
@@ -1,9 +1,64 @@
 import pyaudio, time
+import numpy as np
 
-p = pyaudio.PyAudio()
+# ============== DEFAULT VALUES ==============
 
 chunk_size = 1024
 sample_rate = 44100 
+gpio_pins = [2, 3, 4, 17, 27, 22, 10, 9] 
+
+bands = {
+    "Sub-Bass": (20, 60),
+    "Bass": (60, 150),
+    "Low-Midrange": (150, 400),
+    "Midrange": (400, 1000),
+    "Upper-Midrange": (1000, 2500),
+    "Presence": (2500, 5000),
+    "Brilliance Low": (5000, 10000),
+    "Brilliance High": (10000, 20000),
+}
+
+# ============== UTILITY FUNCTIONS ==============
+
+def calculate_band_sums(magnitude, frame_count, threshold):
+
+    freqs = np.fft.fftfreq(frame_count, 1 / sample_rate)
+    positive_freqs = freqs[:frame_count // 2]    
+
+    binary_number = 0
+    
+    for i, (band_name, (low_freq, high_freq)) in enumerate(bands.items()):
+        # Find indices corresponding to the frequency band
+        band_indices = np.where((positive_freqs >= low_freq) & (positive_freqs <= high_freq))[0]
+        
+        # Sum the magnitudes in this frequency band
+        band_magnitude_sum = np.sum(magnitude[band_indices])
+        
+        if band_magnitude_sum > threshold:
+            binary_number |= (1 << i)  # Set the i-th bit to 1
+    
+    return binary_number
+
+def writeToRegister(value):
+
+    for i, pin in enumerate(gpio_pins):
+        bit = (value >> i) & 1
+        GPIO.output(pin, bit)
+
+def setupGPIO():
+
+    GPIO.setmode(GPIO.BCM)
+
+    for pin in gpio_pins:
+        GPIO.setup(pin, GPIO.OUT)
+
+def shutdownGPIO():
+    GPIO.cleanup()
+
+# ============== MAIN ==============
+
+setupGPIO()
+p = pyaudio.PyAudio()
 
 # Find the loopback device
 for i in range(p.get_device_count()):
@@ -22,12 +77,11 @@ output_stream = p.open(
 
 # Define callback for playback (1)
 def callback(data, frame_count, time_info, status):
-    # audio_signal = np.frombuffer(data, dtype=np.int16)
-    # fft_result = np.fft.fft(audio_signal)                    
-    # magnitude = np.abs(fft_result[:chunk_size // 2])
-    # binary_number = calculate_band_sums(magnitude, chunk_size, 10_000_000)
-    # writeToRegister(binary_number)
-    print(data)
+    audio_signal = np.frombuffer(data, dtype=np.int16)
+    fft_result = np.fft.fft(audio_signal)                    
+    magnitude = np.abs(fft_result[:chunk_size // 2])
+    binary_number = calculate_band_sums(magnitude, chunk_size, 10_000_000)
+    writeToRegister(binary_number)
     output_stream.write(data)
     return (data, pyaudio.paContinue)
 
@@ -52,3 +106,4 @@ input_stream.close()
 output_stream.stop_stream()
 output_stream.close()
 p.terminate()
+shutdownGPIO()
