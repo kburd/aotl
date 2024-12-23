ff9d2b70a02edd8a6dce8c8f26d1f49133395b24
kburd <kalebmburd@gmail.com>
2024-12-23 17:05:51 -0500

    using callback for reading/writing buffers

--- test.py
+++ test.py
@@ -10,6 +10,16 @@ for i in range(p.get_device_count()):
     device_info = p.get_device_info_by_index(i)
     print(device_info)
 
+
+# Open the default output stream
+output_stream = p.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=sample_rate,
+    frames_per_buffer=chunk_size,
+    output=True
+)
+
 # Define callback for playback (1)
 def callback(data, frame_count, time_info, status):
     # audio_signal = np.frombuffer(data, dtype=np.int16)
@@ -18,34 +28,19 @@ def callback(data, frame_count, time_info, status):
     # binary_number = calculate_band_sums(magnitude, chunk_size, 10_000_000)
     # writeToRegister(binary_number)
     print(data)
+    output_stream.write(data)
     return (data, pyaudio.paContinue)
 
 # Open the default input stream
-input_stream = p.open(format=pyaudio.paInt16,
-                      channels=1,
-                      rate=sample_rate,
-                      input=True,
-                      input_device_index=3,
-                      frames_per_buffer=chunk_size)
-
-# Open the default output stream
-output_stream = p.open(format=pyaudio.paInt16,
-                       channels=1,
-                       rate=sample_rate,
-                       output=True,
-                       frames_per_buffer=chunk_size)
-
-# Read and write audio data in chunks
-while True:
-    data = input_stream.read(1024)
-    output_stream.write(data)
-
-# Close the streams and terminate PyAudio
-input_stream.stop_stream()
-input_stream.close()
-output_stream.stop_stream()
-output_stream.close()
-p.terminate()
+input_stream = p.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=sample_rate,
+    frames_per_buffer=chunk_size,
+    input=True,
+    input_device_index=2,
+    stream_callback=callback
+)
 
 
 # # Wait for stream to finish (4)
