7e9dc1d3af3cf8be1b2fd22a25d05c592026e0f1
kburd <kalebmburd@gmail.com>
2024-12-25 06:12:11 -0500

    bug fixes 4

--- src/services/audio.py
+++ src/services/audio.py
@@ -3,38 +3,12 @@ import pyaudio
 from utils.config import config
 from services.register import RegisterService
 
-registerService = RegisterService()
-def process(data, frame_count, time_info, status):
-
-    # Process Audio
-    audio_signal = np.frombuffer(data, dtype=np.int16)
-    fft_result = np.fft.fft(audio_signal)                    
-    magnitude = np.abs(fft_result[:config.chunk_size // 2])
-
-    # Calculate bands
-    freqs = np.fft.fftfreq(frame_count, 1 / config.sample_rate)
-    positive_freqs = freqs[:frame_count // 2] 
-
-    binary_number = 0
-    
-    for i, (band_name, (low_freq, high_freq)) in enumerate(config.bands.items()):
-        band_indices = np.where((positive_freqs >= low_freq) & (positive_freqs <= high_freq))[0]            
-        band_magnitude_sum = np.sum(magnitude[band_indices])
-        if band_magnitude_sum > config.threshold:
-            binary_number |= (1 << i)
-
-    # Output to Register
-    registerService.write(binary_number)
-
-    return (data, pyaudio.paContinue)
-
-
 class AudioService:
 
     def __init__(self):
-        self.registerService = 
         self.p = pyaudio.PyAudio()
         self.stream = None
+        self.registerService = RegisterService()
 
     def start(self):
 
@@ -52,10 +26,34 @@ class AudioService:
             input_device_index=3,
             output=True,
             output_device_index=0,
-            stream_callback=process
+            stream_callback=self.process
         )
 
         self.stream.start_stream()
+
+    def process(self, data, frame_count, time_info, status):
+
+        # Process Audio
+        audio_signal = np.frombuffer(data, dtype=np.int16)
+        fft_result = np.fft.fft(audio_signal)                    
+        magnitude = np.abs(fft_result[:config.chunk_size() // 2])
+
+        # Calculate bands
+        freqs = np.fft.fftfreq(frame_count, 1 / config.sample_rate())
+        positive_freqs = freqs[:frame_count // 2] 
+
+        binary_number = 0
+        
+        for i, (band_name, (low_freq, high_freq)) in enumerate(config.bands().items()):
+            band_indices = np.where((positive_freqs >= low_freq) & (positive_freqs <= high_freq))[0]            
+            band_magnitude_sum = np.sum(magnitude[band_indices])
+            if band_magnitude_sum > config.threshold():
+                binary_number |= (1 << i)
+
+        # Output to Register
+        self.registerService.write(binary_number)
+
+        return (data, pyaudio.paContinue)
 
     def stop(self):
         self.stream.stop_stream()
